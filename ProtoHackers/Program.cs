f5e60954b5380633b63ea5294e77ba88efc57e07
Malik Kennedy <mksybr@gmail.com>
2024-02-09 19:07:13 -0500

    feat: async runner

--- ProtoHackers/Program.cs
+++ ProtoHackers/Program.cs
@@ -1,55 +1,59 @@
 ﻿using System.Net;
-using System.Net.Security;
 using System.Net.Sockets;
-using System.Reflection.Metadata;
-using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
-class Program {
-    private static IPAddress localhost = IPAddress.Parse("127.0.0.1"); 
-
-    static void Main(string[] args)
+class Program
+{
+    private static IPAddress localhost = IPAddress.Parse("127.0.0.1");
+    private static Int32 port = 1045;
     private static ILogger logger;
+    static void Main(string[] args)
     {
-        // Display the number of command line arguments.
-        Int32 port = Int32.Parse(args[1]) ?? 443;
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        Int32 solution = 1;
+        TcpListener server = new TcpListener(localhost, port);
         using ILoggerFactory loggingFactory = LoggerFactory.Create(builder => builder.AddConsole());
         logger = loggingFactory.CreateLogger("ProtoHackers/Solve1");
-        logger.LogInformation(args[0]);
-        _ = typeof(Program).GetMethod("Solve" + args[0]).Invoke(null, []);
+        string index = solution.ToString();
+        _ = typeof(Program).GetMethod(name: "Solve" + index)
+                           .Invoke(null, [server, 1045]);
     }
 
-    async public static Task Solve0(Int32 port = 1025)
+    async public static Task Solve0(TcpListener server, Int32 port = 1025)
     {
-        TcpListener server = null;
         logger.LogInformation("Connected. ");
         try
         {
             server.Start();
-            Byte[] bytes = new Byte[4096];
-            String? data = null;
-            while(true) {
-                using TcpClient client = server.AcceptTcpClient();
-                data = null;
-                NetworkStream stream = client.GetStream();
-                int i ;
-                while((i = stream.Read(bytes, 0, bytes.Length))!=0)
             logger.LogInformation($"Created Tcp Server on {localhost.ToString()}:{port}");
             while (true)
             {
                 logger.LogInformation("Waiting for connection. ");
                 using TcpClient client = await server.AcceptTcpClientAsync();
+                await Task.Run(() =>
                 {
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                    stream.Write(msg, 0, msg.Length);
-                }
+                    Byte[] buffer = new Byte[4096];
+                    String data = null;
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    while ((i = stream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(buffer, 0, i);
                         logger.LogInformation($"Recv: {data}");
+                        byte[] returnMessage = System.Text.Encoding.ASCII.GetBytes(data);
+                        stream.Write(returnMessage, 0, returnMessage.Length);
                         logger.LogInformation($"Send: {data}");
+                    }
+                });
             }
         }
-        catch(SocketException e)
+        catch (SocketException e)
         {
             logger.LogCritical($"{e}");
         }
@@ -58,41 +62,51 @@ class Program {
             server.Stop();
         }
     }
-    public static void Solve1(Int32 port)
+    public static bool isPrime(Decimal number)
+    {
+        return false;
+    }
+
+    public static void Solve1(TcpListener server, Int32 port)
     {
         logger.LogInformation("Solve1");
-        TcpListener server = null;
-        try {
-            server = new TcpListener(port);
+        try
+        {
+            server = new TcpListener(localhost, port);
             server.Start();
-            Byte[] bytes = new Byte[4096];
-            String? data = null;
-            while(true) {
+            while (true)
+            {
                 using TcpClient client = server.AcceptTcpClient();
-                data = null;
+                String? data = null;
                 NetworkStream stream = client.GetStream();
-                int i ;
-                while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+                int i;
+                Byte[] bytes = new Byte[4096];
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
                     data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-                    var datas = data.Split ("\n");
-                    datas.ToList().ForEach (data => {
-                        try {
-                            json = JsonDocument.Parse(data);
-                            bool isPrime = json.isPrime;
-                            Numeric number = Numeric.Parse(json.number);
-                            return isPrime && isPrime(number));
-                        } catch {}
-                        
+                    var datas = data.Split("\n");
+                    datas.ToList().ForEach(data =>
+                    {
+                        var json = JsonDocument.Parse(data).RootElement;
+                        bool isPrime = json.GetProperty("isPrime").GetBoolean();
+                        decimal number = json.GetProperty("number").GetDecimal();
+                        if (isPrime
+                            && Program.isPrime(number))
+                        {
+                            // TODO send back message
+                        }
+                        else
+                        {
+                            // TODO send back message
+                        }
+                        stream.Write(System.Text.Encoding.ASCII.GetBytes(data), 0, System.Text.Encoding.ASCII.GetBytes(data).Length);
                     });
-                    stream.Write(msg, 0, msg.Length);
-                            logger.LogCritical("SocketException: {0}", e);
                 }
             }
         }
-        catch(SocketException e)
+        catch (SocketException e)
         {
+            logger.LogCritical("SocketException: {0}", e);
         }
         finally
         {
@@ -100,3 +114,4 @@ class Program {
         }
     }
 }
+
