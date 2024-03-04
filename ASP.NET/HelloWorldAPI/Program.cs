03b5bfbffaf09d4b3debe4185a40b5f5c518f7bc
Malik Kennedy <mksybr@gmail.com>
2024-03-04 12:21:20 +0000

    feat: minimal api example

---
+++ ASP.NET/HelloWorldAPI/Program.cs
@@ -0,0 +1,62 @@
+using HelloWorldAPI.Helpers;
+using System.Text.Json.Serialization;
+
+namespace HelloWorldAPI;
+
+public class Program
+{
+  public static async Task<int> Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    var services = builder.Services;
+    // Add services to the container.
+
+    services.AddSingleton<DataContext>()
+            .AddCors()
+            .AddControllers()
+            .AddJsonOptions(options =>
+            {
+              // serialize enums as strings
+              options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+              
+              // ignore omitted parameters on models to enable optional params 
+              options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    // services.AddScoped<IUserRepository, UserRepository>();
+    // services.AddScoped<IUserService, UserService>();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer()
+            .AddSwaggerGen();
+
+    var app = builder.Build();
+    {
+      using var scope = app.Services.CreateScope();
+      var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+      await context.Init();
+    }
+
+    {
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+
+      app.UseCors(options =>
+      options.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+
+      app.UseMiddleware<ErrorHandlerMiddleware>();
+
+      app.UseAuthorization();
+
+      app.MapControllers();
+    }
+    app.Run();
+    return 0;
+  }
+}
